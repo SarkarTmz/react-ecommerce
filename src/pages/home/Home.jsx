634ef62aa3da0da520dfbef3a907bce24118be49
Sarkar <ttpone333@gmail.com>
2024-04-08 22:12:15 +0545

    Updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,16 @@
 import React, { useContext} from "react";
 import Layout from "../../components/layout/Layout";
 import myContext from "../../context/data/myContext";
+import HeroSection from "../../components/heroSection/HeroSection";
+import Filter from "../../components/filter/Filter";
 
 const Home = () => {
   return (
     <div>
-      <Layout>Home</Layout>
+      <Layout>
+        <HeroSection/>
+        <Filter/>
+      </Layout>
     </div>
   );
 };
